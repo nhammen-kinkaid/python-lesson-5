6f904bc8f858ccde8baf6d6eeafb904b7940a821
nhammen-kinkaid <nathaniel.hammen@kinkaid.org>
2026-06-16 16:41:24 +0000

    Explicitly initialize list

--- slicing.py
+++ slicing.py
@@ -1,8 +1,8 @@
-my_list = range(10)
+my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 print(my_list)
 print(my_list[2:5])
 new_list = my_list[-5:-2]
 print(new_list)
-my_list[-5:-2] = 0
+my_list[-5:-2] = [0]
 print(my_list)
 print(new_list)
